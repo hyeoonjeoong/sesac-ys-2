3b579e6f261a9ab88ac895a79482e8ecf64336db
hyeoonjeoong <jeooooong22@gmail.com>
2023-12-12 23:36:19 +0900

    리액트 연습

--- react-proj/08-redux/src/containers/Money.js
+++ react-proj/08-redux/src/containers/Money.js
@@ -2,23 +2,34 @@ import { useState } from "react";
 
 export function Money() {
   const [money, setMoney] = useState(0);
+  const [inputValue, setInputValue] = useState("");
 
-  const addMoney = (value) => {
-    const newMoney = value + money;
-    console.log(newMoney);
+  const addMoney = () => {
+    const inputCash = inputValue;
+    if (inputCash) {
+      setMoney((prevMoney) => prevMoney + inputCash);
+
+      setInputValue("");
+    }
   };
 
+  const minusMoney = () => {
+    const outputCash = inputValue;
+    if (outputCash) {
+      setMoney((prevMoney) => prevMoney - outputCash);
+    }
+  };
   return (
     <>
       <h3>코딩온 은행</h3>
       <h2>잔액 : {money}원</h2>
       <input
         type="number"
-        value={money}
-        onChange={(e) => setMoney(e.target.value)}
+        value={inputValue}
+        onChange={(e) => setInputValue(parseFloat(e.target.value))}
       />
       <button onClick={addMoney}>입금</button>
-      <button>출금</button>
+      <button onClick={minusMoney}>출금</button>
     </>
   );
 }
